8e226298569ff5ab1a005c9877579bcfaaa2d9d4
tpluscode <tpluscode@users.noreply.github.com>
2021-06-07 20:17:41 +0200

    refactor: make extension for @zazuko/rdf-vocabularies

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-import { expandWithCheck } from './expandWithCheck'
-import { expand as _expand } from './expand'
-import { NamedNode } from 'rdf-js'
+import { prefixes } from '@zazuko/rdf-vocabularies'
+import morePrefixes from './prefixes'
 
-export { default as prefixes } from './prefixes'
 export { vocabularies, loadFile } from './vocabularies'
-export { shrink } from './shrink'
-export { expandWithCheck } from './expandWithCheck'
 
-export function expand (prefixed: string): string;
-export function expand (prefixed: string, types: (string | NamedNode)[]): Promise<string>;
-export function expand (prefixed: string, types: (string | NamedNode)[] = []): string | Promise<string> {
-  if (types && types.length) {
-    return expandWithCheck(prefixed, types)
-  }
-
-  return _expand(prefixed)
-}
+Object.entries(morePrefixes)
+  .forEach(([prefix, namespace]) => {
+    prefixes[prefix] = namespace
+  })
--- src/prefixes.ts
+++ src/prefixes.ts
@@ -1,13 +1,5 @@
 const prefixes = {
-  // Do not remove these prefixes. They are necessary for building the package
-  dbo: 'http://dbpedia.org/ontology/',
-  dc11: 'http://purl.org/dc/elements/1.1/',
-  dcterms: 'http://purl.org/dc/terms/',
-  rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-  rdfa: 'http://www.w3.org/ns/rdfa#',
-  rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
-
-  // Add your prefixes here
+  // Replace with your prefixes here
   ex: 'https://example.com/vocab#'
 }
 
