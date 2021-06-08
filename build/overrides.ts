770599e61e7300c5a625bf6fb8c72c9aba10b418
tpluscode <tpluscode@users.noreply.github.com>
2021-06-08 08:03:16 +0200

    chore: no need for the overrides

--- build/overrides.ts
+++ build/overrides.ts
@@ -14,13 +14,5 @@ export interface Override extends FileSpec {
 export const overrides: Record<string, Override> = {
   ex: {
     file: 'file:src/vocabulary/ex.ttl'
-  },
-  // do not process the standard vocabularies
-  // import them from `@zazuko/rdf-vocabularies`
-  dbo: { skip: true },
-  dc11: { skip: true },
-  dcterms: { skip: true },
-  rdf: { skip: true },
-  rdfa: { skip: true },
-  rdfs: { skip: true }
+  }
 }
