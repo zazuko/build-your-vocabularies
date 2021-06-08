e320f83801a50b5e178d5cdf88d541a87bab0225
tpluscode <tpluscode@users.noreply.github.com>
2021-06-08 07:57:39 +0200

    refactor: simpler prefixes module

--- src/prefixes.ts
+++ src/prefixes.ts
@@ -1,13 +1,4 @@
-const prefixes = {
+export default {
   // Replace with your prefixes here
   ex: 'https://example.com/vocab#'
 }
-
-type KnownPrefixes = {
-  readonly [key in keyof typeof prefixes]: string
-}
-interface CustomPrefixes {
-  [key: string]: string;
-}
-
-export default prefixes as KnownPrefixes & CustomPrefixes
